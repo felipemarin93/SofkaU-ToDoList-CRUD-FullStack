437d6d4730ae70ec3250c0add0941b86dfaa99c2
felipemarin93 <felipemaringiraldo@gmail.com>
2022-06-17 16:56:09 -0500

    Se agrega la funcionalidad para el método de eliminar las subtareas de la Base de datos

--- frontEnd/js/seccion.js
+++ frontEnd/js/seccion.js
@@ -4,8 +4,8 @@ export const fillSection =(titleList,idList,e) =>{
         <div class="card border-info mb-3" style="max-width: 80rem;">
             <div class="card-header">
                 <h2 >${titleList}</h2>
-                    <input type="text" placeholder="new task" style="width : 250px">
-                    <button class= "createTask btn btn-success my-2 my-sm-0" type="submit" value="${idList}">Add Task</button>
+                    <input type="text" placeholder="new task" style="width : 250px"></input>
+                    <button class= "createTask btn btn-success my-2 my-sm-0" type="submit" value="${idList}" >Add Task</button>
                     <spam  id="novisible"class = "spamId">${idList}</spam>
                     <button class= "deleteList btn btn-danger my-2 my-sm-0" " type="submit" style="margin-left: 200px" value="${idList}">Delete
                 List</button>
@@ -42,7 +42,7 @@ export const fillSection2 =(titleList,idList,tarea) =>{
     return `<div class="container-task">
         <div class="card border-info mb-3" style="max-width: 80rem;">
             <div class="card-header">
-                <h2 >${titleList}</h2>
+                <h1 >${titleList}</h1>
                     <input type="text" placeholder="new task" style="width : 250px">
                     <button class= "createTask btn btn-success my-2 my-sm-0" type="submit" value="${idList}">Add Task</button>
                     <spam  id="novisible"class = "spamId">${idList}</spam>
@@ -52,9 +52,9 @@ export const fillSection2 =(titleList,idList,tarea) =>{
         <div class="card-body">
             <table id="tabla-tarea" class="table table-hover">
                 <tr>
-                    <th>Task Name</th>
-                    <th>Completed</th>
-                    <th>Actions</th>
+                    <th><h5>Task Name</h5></th>
+                    <th><h5>Completed</h5></th>
+                    <th><h5>Actions</h5></th>
 
                 </tr>
                 ${tarea}
@@ -66,50 +66,6 @@ export const fillSection2 =(titleList,idList,tarea) =>{
     
 }
 
-
-export const fillSectionx = (listas) => {
-    let resultadoSub = "";
-    let resultado="";
-    listas.forEach(lista => {  
-        resultadoSub=''
-        lista.tareas.forEach(task => {
-            resultadoSub += `<tr>
-            <th>TaskName</th>
-            <th>Completed</th>
-            <th>Actions</th>
-
-        </tr>
-        <tr>
-            <td>${task.nombre}</td>
-            <td>
-                <input class="form-check-input" type="checkbox" value="" id="checkbox"
-                    style="text-align:right">
-            </td>
-            <td><button class=" editTask btn btn-warning" id="editTask${task.id}">Edit Task</button>
-            <button class=" deleteTask btn btn-danger" id="editTask${task.id}">Delete Task</button>
-        </td>
-    </tr>`
-
-        })
-        resultado += 
-        `<div class="container-task">
-        <div class="card border-info mb-3" style="max-width: 80rem;">
-            <div class="card-header">
-                <h2 >${titleList}</h2>
-                    <input type="text" placeholder="new task" style="width : 250px">
-                    <button class= "createTask btn btn-success my-2 my-sm-0" type="submit" value="${idList}">Add Task</button>
-                    <spam  id="novisible"class = "spamId">${idList}</spam>
-                    <button class= "deleteList btn btn-danger my-2 my-sm-0" " type="submit" style="margin-left: 200px" value="${idList}">Delete
-                List</button>
-        </div>
-        <div class="card-body">
-            ${resultadoSub}
-            <table id="tabla-tarea" class="table table-hover"></table>`
-
-    })
-
-
-}
 
 
 
--- frontEnd/js/todoapp.js
+++ frontEnd/js/todoapp.js
@@ -1,10 +1,8 @@
-let url = 'http://localhost:8080';
+let url = "http://localhost:8080";
 
-import {fillSection, fillSection2} from "./seccion.js"
-const $container_task = document.querySelector('.container-task');
-const $createList = document.querySelector('#btnAddList');
-let result = "";
-let resultTask="";
+import { fillSection, fillSection2 } from "./seccion.js";
+const $container_task = document.querySelector(".container-task");
+const $createList = document.querySelector("#btnAddList");
 
 
 /**
@@ -12,203 +10,165 @@ let resultTask="";
  * método showList para mostralas
  */
 
+fetch(url + `/lista`)
+    .then((response) => response.json())
+    .then((cardJson) => showList(cardJson))
+    .catch((error) => alert(error.message));
 
+/**
+ * Función que obtiene las listas en el DOM con sus respectivas listas y tareas,
+ * además recorre cada element de dicho arreglo obtenido para obtener el nombre y el id de las listas y las tareas
+ * Por último agrega cada card para cada lista en el HTML principal
+ */
 
-
-
- fetch(url+`/lista`)
-     .then(response=>response.json())
-     .then(cardJson=>showList(cardJson))
-     .catch(error=>alert(error.message));
-
-     /**
-    * Función que obtiene las listas en el DOM con sus respectivas listas,
-    * además recorre cada element de dicho arreglo obtenido para obtener el nombre y el id de las listas
-    * Por último agrega cada card para cada lista en el HTML principal
-    */ 
-
-      const showList = async (cards)=>{
-          let card = "";
-          let tarea ="";
-           await cards.forEach(e => {
-
-            tarea ="";
-            e.tarea.forEach(task => {
-                
-                tarea+= `
+const showList = async (cards) => {
+    let card = "";
+    let tarea = "";
+    await cards.forEach((e) => {
+        tarea = "";
+        e.tarea.forEach((task) => {
+            tarea += `
                 <tr>
                     <td>${task.nombre}</td>
                     <td>
                         <input class="form-check-input" type="checkbox" value="" id="checkbox"
                             style="text-align:right">
                     </td>
-                    <td><button class="btn btn-warning">Edit Task</button>
-                        <button class="btn btn-danger">Delete Task</button>
+                    <td><button class="editTask btn btn-warning" value="${task.id}">Edit</button>
+                        <button class="deleteTask btn btn-danger" value="${task.id}">Delete</button>
                     </td>
-                </tr>`
-            });
+                </tr>`;
+        });
 
-                card+= fillSection2(e.nombre, e.id,tarea);
-           });
+        card += fillSection2(e.nombre, e.id, tarea);
+    });
 
-            fillSection(cards);
-          $container_task.innerHTML = card;  
-        
-      }
-
-    //   const showList = async (cards)=>{
-    //     let card = "";
-    //      await cards.forEach(e => {
-
-    //           card+= fillSection(e.nombre, e.id);
-    //      });
-
-    //       fillSection(cards);
-    //     $container_task.innerHTML = card;  
-      
-    // }
-
-    
-
-
-
-
-    /**
-    * Función de evento del click para agregar lista capturando el valor que tiene el input del
-    * HTML, y llama a la función crear enviandole dicho valor, por último recarga la página para actualizar los cambios.
-    */   
-     $createList.addEventListener('click', e => {
-        e.preventDefault();
-        createList(document.getElementById('list-name').value)
-        location.reload();
-    
-     })
+    fillSection(cards);
+    $container_task.innerHTML = card;
+};
 
 /**
- * Esta función de  crear Lista recibe como paramentro el arreglo y lo transforma en JSON para enviar la 
+ * Función de evento del click para agregar lista capturando el valor que tiene el input del
+ * HTML, y llama a la función crear enviandole dicho valor, por último recarga la página para actualizar los cambios.
+ */
+$createList.addEventListener("click", (e) => {
+    e.preventDefault();
+    let listName = document.getElementById("list-name").value;
+
+    if (listName.trim() === "") {
+        Swal.fire(
+            "El campo de la lista está vacío, favor ingresa un nombre válido"
+        );
+        return;
+    }
+    createList(listName);
+    location.reload();
+});
+
+/**
+ * Esta función de  crear Lista recibe como paramentro el arreglo y lo transforma en JSON para enviar la
  * solicitud de tipo POST al backend para insertar un nuevo registro en la entidad de Listas, por ultima trae la información
  * y refresca la página.
  */
-   
-      async function createList(newLista) {
-            
-             let informationReceipt = {
-                 method: "POST",
-                 headers: {
-                     "Content-type": "application/json; charset=utf-8"
-                 },
-                 body: JSON.stringify({
-                     nombre: newLista
-                 })
-             },
-                 res = await fetch(`${url}/crearlista`, informationReceipt)
 
-                showList();
-         } 
-         
+async function createList(newLista) {
+    let informationReceipt = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+            nombre: newLista,
+        }),
+    },
+        res = await fetch(`${url}/crearlista`, informationReceipt);
 
-         /**
-          * Evento que escucha los clicks que se hacen dentro del contenedor, cuando clickeas
-          * en el botón eliminar llama el método eliminar tarea y cuando se clickea en create tarea
-          * llama la creación de dicho elemento.
-          */
-         $container_task.addEventListener("click", async (e) => {
-            if (e.target.classList[0] == "deleteList") {
-               deleteList(e.target.previousElementSibling.textContent)
-              
-            }
-            if (e.target.classList[0] == "createTask") { 
-                e.preventDefault()
-                console.log(e.path[0].value);
-                //if (e.target.parentElement.children[3].textContent == "Crear") {
-                  let dato = {
-                    nombre:e.target.previousElementSibling.value,
-                    id:e.path[0].value
-                  }
-                  
-                  createTask(dato.nombre, dato.id)   
-                
-                    
-              }
-        
-        })
-        /**
-         * Función para eliminar una lista recibiendo el
-         * @param {*} id que es capturado por el evento listener
-         * por ultimo recarga la pagina
-         */
-        async function deleteList(id) {
-            
-            let informationReceipt = {
-                method: "DELETE",
-                headers: {
-                    "Content-type": "application/json; charset=utf-8"
-                },
-            },
-                res = await fetch(`${url}/eliminarlista/${id}`, informationReceipt)
-        
-            location.reload()
-            
-        }   
-         
-
-    
-/**
- * Esta función muestra las listas almacenadas en la base de datos usando una solicitud tipo  GET
- * en la cual se concatena la URL mas la ruta especifica que tiene el endpoint del backend para traer la información completa
- * 
- */
-
-
- const showTask = (tasks) =>{
-     let card = "";
-     tasks.forEach(element => {
-         card+= fillSection(element.nombre);
-     });
-     $container_task.innerHTML = card;
- }
-
-
-
-
- async function createTask(nombre,id){
-    
-        let informationReceipt = {
-            method: "POST",
-            headers: {
-              "Content-type": "application/json; charset=utf-8"
-            },
-            body: JSON.stringify({  
-              nombre: nombre,
-              completado: false,
-              lista:{
-                      id: id
-                  }
-            })
-            
-          },
-        
-            res = await fetch(`${url}/creartarea`, informationReceipt)
-            location.reload()
-            
-  
+    showList();
 }
 
-async function deleteTask(id) {
-            
+/**
+ * Evento que escucha los clicks que se hacen dentro del contenedor, cuando clickeas
+ * en el botón eliminar llama el método eliminar tarea y cuando se clickea en create tarea
+ * llama la creación de dicho elemento.
+ */
+$container_task.addEventListener("click", async (e) => {
+    if (e.target.classList[0] == "deleteList") {
+        deleteList(e.target.previousElementSibling.textContent);
+         
+
+    }
+    if (e.target.classList[0] == "createTask") {
+        e.preventDefault();
+        console.log(e.path[0].value);
+        //if (e.target.parentElement.children[3].textContent == "Crear") {
+        let dato = {
+            nombre: e.target.previousElementSibling.value,
+            id: e.path[0].value,
+        };
+
+        createTask(dato.nombre, dato.id);
+    }
+    if (e.target.classList[0] == "deleteTask") {
+        deleteTask(e.path[0].value);
+        
+    }
+});
+/**
+ * Función para eliminar una lista recibiendo el
+ * @param {*} id que es capturado por el evento listener
+ * por ultimo recarga la pagina
+ */
+async function deleteList(id) {
     let informationReceipt = {
         method: "DELETE",
         headers: {
-            "Content-type": "application/json; charset=utf-8"
+            "Content-type": "application/json; charset=utf-8",
         },
     },
-        res = await fetch(`${url}/eliminartarea/${id}`, informationReceipt)
+        res = await fetch(`${url}/eliminarlista/${id}`, informationReceipt);
 
-    location.reload()
-    
-}   
-
+    location.reload();
+}
 
 
 
 
+/**
+ * Esta función crea una tarea en la base de datos recibiendo los parametros de nombre y el id
+ * por lo cual, dicha información se transforma en un json para enviarlo al endpoint crear tarea.
+ * Por último se recarga la página
+ */
+async function createTask(nombre, id) {
+    let informationReceipt = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+            nombre: nombre,
+            completado: false,
+            lista: {
+                id: id,
+            },
+        }),
+    },
+        res = await fetch(`${url}/creartarea`, informationReceipt);
+    location.reload();
+}
+
+/**
+ * Esta función elimina una tarea de la base de datos
+ * Luego recarga la página
+ */
+
+async function deleteTask(id) {
+    let informationReceipt = {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset=utf-8",
+        },
+    },
+        res = await fetch(`${url}/eliminartarea/${id}`, informationReceipt);
+
+    location.reload();
+}
